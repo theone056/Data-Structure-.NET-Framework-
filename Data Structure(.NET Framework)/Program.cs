7dc32e1203cb1146af89782756585084d0a45847
theone056 <32253665+theone056@users.noreply.github.com>
2021-09-19 14:46:14 +0800

    Added new Problems

--- Data Structure(.NET Framework)/Program.cs
+++ Data Structure(.NET Framework)/Program.cs
@@ -10,9 +10,9 @@ namespace Data_Structure_.NET_Framework_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(ProblemSeven("abcd"));
-            Console.WriteLine(ProblemSeven("a"));
-            Console.WriteLine(ProblemSeven("xy"));
+            Console.WriteLine(ProblemTen(3));
+            Console.WriteLine(ProblemTen(14));
+            Console.WriteLine(ProblemTen(37));
         }
 
         //Basic Algoritm
@@ -81,7 +81,28 @@ namespace Data_Structure_.NET_Framework_
             return input.Length > 1 ? input.Substring((input.Length - 1), 1) + input.Substring(1, (input.Length - 2)) + input.Substring(0, 1) : input;
         }
 
+        /*Write a C# Sharp program to create a new string which is 4 copies of the 2 front characters of a given string. 
+         If the given string length is less than 2 return the original string.*/
+
+        private static string ProblemEight(string input)
+        {
+            return input.Length >= 2 ? input.Substring(0,2) + input.Substring(0, 2) + input.Substring(0, 2) + input.Substring(0, 2) : input;
+        }
 
 
+        /*Write a C# Sharp program to create a new string with the last char added at the front and back of a given string of length 1 or more.*/
+
+        private static string ProblemNine(string input)
+        {
+            var lastchar =  input.Substring((input.Length-1), 1);
+            return input.Length >= 1 ? lastchar + input + lastchar : input;
+        }
+
+        /*Write a C# Sharp program to check if a given positive number is a multiple of 3 or a multiple of 7*/
+
+        private static bool ProblemTen(int num)
+        {
+            return num % 3 == 0 || num % 7 == 0 ? true : false; 
+        }
     }
 }
